002550851cce051e644b73c36035c12a761688b5
KatKing <kathrynking.1987@gmail.com>
2016-06-04 09:32:55 -0700

    Added jQuery Lidghtbox plugin.

--- js/app.js
+++ js/app.js
@@ -1,70 +1,67 @@
 $(document).ready(function(){
-	$('nav a').on("click", function(){
-		//current class assignment
+	$('nav a').on('click', function(){
+		// Current class assigniment
 		$('nav li.current').removeClass('current');
 		$(this).parent().addClass('current');
-		console.log(this);
-
-		//Set the heading text
+		
+		// Set heading text
 		$('h1#heading').text($(this).text());
-
-
-		//Get and Filter Link Text
-		var category = $(this).text().toLowerCase().replace(' ', '-');
-
-
-		//remove hidden class if "all-projects" is selected
-		if(category === 'all-projects'){
+		
+		// Get & filter link text
+		var category = $(this).text().toLowerCase().replace(' ','-');
+		
+		// Remove hidden class if 'all-projects' is selected
+		if(category == 'all-projects'){
 			$('ul#gallery li:hidden').fadeIn('slow').removeClass('hidden');
 		} else {
 			$('ul#gallery li').each(function(){
 				if(!$(this).hasClass(category)){
 					$(this).hide().addClass('hidden');
-				}else {
+				} else {
 					$(this).fadeIn('slow').removeClass('hidden');
 				}
-				
 			});
-				
 		}
-		//Stop link behavior
+		// Stop link behaviour
 		return false;
-
 	});
-
-	$('ul#gallery li').on('mouseenter', function(){
-		//Get Data attribute values
+	
+	// Mouseenter Overlay Effect
+	$('ul#gallery li').on('mouseenter',function(){
+		// Get data attribute values
 		var title = $(this).children().data('title');
-		var description = $(this).children().data('description');
+		var desc = $(this).children().data('desc');
 
-		//Validation
-		if(description === null){
-			description = 'Click to Enlarge';
-		}
-		if (title === null) {
-			title = " ";
+		if(desc == null){
+			desc = 'Click To Enlarge';
 		}
 
+		if(title == null){
+			title = '';
+		}
+
+		// Create an overlay div
 		$(this).append('<div class="overlay"></div>');
 
-		//Get the overlay div
-
+		// Get the overlay div
 		var overlay = $(this).children('.overlay');
 
-		//Add html to overlay 
-		overlay.html('<h2>'+title+'</h2><p>'+description+'</p>');
+		// Add html to overlay
+		overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
 
+		// Fade in overlay
 		overlay.fadeIn(800);
-		console.log('bye');
 	});
-	//Mouseleave Overlay
-	$('ul#gallery li').on('mouseleave', function(){
+
+	// Mouseleave Overlay Effect
+	$('ul#gallery li').on('mouseleave',function(){
+		// Create an overlay div
 		$(this).append('<div class="overlay"></div>');
+
+		// Get the overlay div
 		var overlay = $(this).children('.overlay');
-		overlay.fadeOut(800);
-		console.log('hello');
 
-	})
-
-
+		// Fade out overlay
+		overlay.fadeOut(500);
+	});
 });
